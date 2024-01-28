b2345f618838be83c16cb24c2cb4f3db5b562952
supawat_thinkpad <enlighten.rf@gmail.com>
2024-01-28 19:48:25 +0700

    use 2 ecard for usb passthrough

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
 DigitalOut led(LED1);
 TimerTPL5010 tpl5010(WDT_WAKE_PIN, WDT_DONE_PIN);
 UnbufferedSerial ecard(ECARD_TX_PIN, ECARD_RX_PIN, 2400);
+UnbufferedSerial ecard2(MDM_TXD_PIN, MDM_RXD_PIN, 2400);
 USBSerial pc;
 
 Thread isr_thread(osPriorityAboveNormal, 0x400, nullptr, "isr_queue_thread");
@@ -23,6 +24,7 @@ Thread usb_thread;
 EventQueue isr_queue;
 
 void on_rx_interrupt();
+void on_rx_interrupt2();
 
 void usb_task() {
   printf("Start usb_task\r\n");
@@ -33,6 +35,7 @@ void usb_task() {
         uint8_t byte = pc.getc();
         // putc(byte, stdout);
         ecard.write((void *)&byte, 1);
+        ecard2.write((void *)&byte, 1);
       }
     }
   }
@@ -44,6 +47,7 @@ int main() {
   isr_thread.start(callback(&isr_queue, &EventQueue::dispatch_forever));
   tpl5010.init(&isr_queue);
   ecard.attach(callback(on_rx_interrupt), SerialBase::RxIrq);
+  ecard2.attach(callback(on_rx_interrupt2), SerialBase::RxIrq);
   usb_thread.start(callback(usb_task));
 
   while (true) {
@@ -61,6 +65,16 @@ void on_rx_interrupt() {
 
   // Read the data to clear the receive interrupt.
   if (ecard.read(&c, 1)) {
+    // Echo the input back to the terminal.
+    pc.write((void *)&c, 1);
+  }
+}
+
+void on_rx_interrupt2() {
+  char c;
+
+  // Read the data to clear the receive interrupt.
+  if (ecard2.read(&c, 1)) {
     // Echo the input back to the terminal.
     pc.write((void *)&c, 1);
   }
