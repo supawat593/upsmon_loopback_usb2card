c83161d70c8bafdd67065fe63cd2e3fc93e40119
Supawat_msi <enlighten.rf@gmail.com>
2024-01-28 15:37:30 +0700

    initial commit one board

---
+++ devices_src.h
@@ -0,0 +1,129 @@
+#if TARGET_K22F
+#define MDM_TXD_PIN PTD3
+#define MDM_RXD_PIN PTD2
+#define MDM_RI_PIN PTD1
+#define MDM_DTR_PIN PTD4
+
+#define MDM_VRF_EN_PIN PTC8
+#define MDM_RST_PIN PTB1
+#define MDM_PWR_PIN PTB2
+#define MDM_FLIGHT_PIN PTC10
+#define MDM_STATUS_PIN PTC9
+
+#define ECARD_TX_PIN PTB17
+#define ECARD_RX_PIN PTB16
+
+#define WDT_WAKE_PIN PTC0
+#define WDT_DONE_PIN PTB19
+#define USB_DET_PIN PTA12
+
+#define DIPSW_P1_PIN PTC4
+#define DIPSW_P2_PIN PTC5
+#define DIPSW_P3_PIN PTC6
+#define DIPSW_P4_PIN PTC7
+
+#endif
+
+#if TARGET_LPC1768
+#define MDM_TXD_PIN P0_15
+#define MDM_RXD_PIN P0_16
+#define MDM_RI_PIN P0_21
+#define MDM_DTR_PIN P0_20
+
+#define MDM_VRF_EN_PIN P1_29
+#define MDM_RST_PIN P1_14
+#define MDM_PWR_PIN P1_10
+#define MDM_FLIGHT_PIN P1_25
+#define MDM_STATUS_PIN P1_24
+
+#define ECARD_TX_PIN P0_10
+#define ECARD_RX_PIN P0_11
+
+#define WDT_WAKE_PIN P2_1
+#define WDT_DONE_PIN P2_0
+#define USB_DET_PIN P1_31
+
+#define DIPSW_P1_PIN P2_3
+#define DIPSW_P2_PIN P2_4
+#define DIPSW_P3_PIN P2_5
+#define DIPSW_P4_PIN P2_6
+
+#endif
+
+#if TARGET_NUCLEO_F401RE
+#define MDM_TXD_PIN PC_6
+#define MDM_RXD_PIN PC_7
+#define MDM_RI_PIN PC_8
+#define MDM_DTR_PIN PC_9
+
+#define MDM_VRF_EN_PIN PA_8
+#define MDM_PWR_PIN PB_12
+#define MDM_RST_PIN PB_13
+#define MDM_STATUS_PIN PB_14
+#define MDM_FLIGHT_PIN PB_15
+
+#define ECARD_TX_PIN PB_6
+#define ECARD_RX_PIN PB_7
+
+#define WDT_WAKE_PIN PA_0
+#define WDT_DONE_PIN PA_1
+#define USB_DET_PIN PC_13
+
+#define DIPSW_P1_PIN PC_4
+#define DIPSW_P2_PIN PC_5
+#define DIPSW_P3_PIN PB_0
+#define DIPSW_P4_PIN PB_1
+
+#endif
+
+#define firmware_vers "661115"
+#define Dev_Group "LTE"
+
+#define INITIAL_APP_FILE "initial_script.txt"
+#define SPIF_MOUNT_PATH "spif"
+#define FULL_SCRIPT_FILE_PATH "/" SPIF_MOUNT_PATH "/" INITIAL_APP_FILE
+
+char mqtt_broker_ip[] = "188.166.189.39";
+// char mqtt_broker[] = "trueiot.io";
+// int mqtt_port = 1883;
+char mqtt_usr[] = "IoTdevices";
+char mqtt_pwd[] = "devices@iot";
+// char model_Name[] = "Chuphotic1";
+// char site_ID[] = "1234567890";
+
+// const char *str_cmd[] = {"Q1", "Q4", "QF"}; // 1phase
+const char *str_ret[] = {
+    "(221.0 204.0 219.0 000 50.9 2.26 27.0 00000000",
+    "(222.7 000.0 000.0 204.0 222.0 000 000 50.9 382 384 108.4 27.0 IM",
+    "(07 204.0 49.8 208.6 50.3 152 010.5 433 414 100.2 03.4 01111111",
+    "NO_RESP1",
+    "NO_RESP2",
+    "NO_RESP3"};
+
+char init_cfg_pattern[] = {"%*[^\n]\nBroker: \"%[^\"]\"\nPort: %d\nKey: "
+                           "\"%[^\"]\"\nTopic: \"%[^\"]\"\nCommand: "
+                           "[%[^]]]\nModel: \"%[^\"]\"\nSite_ID: "
+                           "\"%[^\"]\"\n%*s"};
+
+char init_cfg_write[] = {
+    "#Configuration file for UPS Monitor\r\n\r\nSTART:\r\nBroker: "
+    "\"%s\"\r\nPort: %d\r\nKey: \"%s\"\r\nTopic: \"%s\"\r\nCommand: "
+    "[%s]\r\nModel: \"%s\"\r\nSite_ID: \"%s\"\r\nSTOP:"};
+
+char payload_pattern[] = "{\"imei\":%s,\"utc\":%u,\"model\":\"%s\",\"site_ID\":"
+                         "\"%s\",\"cmd\":\"%s\",\"resp\":\"%s\"}";
+
+char stat_pattern[] =
+    "{\"imei\":%s,\"utc\":%u,\"firm_vers\":\"%s\",\"dev_group\":"
+    "\"%s\",\"period_min\":%u,\"stat_mode\":"
+    "\"%s\",\"csq_stat\":\"+CSQ: "
+    "%d,%d\",\"cops_opn\":\"%s\",\"reg_stat\":\"%s\",\"ext_stat\":\"%s\"}";
+
+char dummy_msg[] =
+    "(224.4 000.0 000.0 204.0 220.5 000 000 50.9 385 380 108.4 24.0 IM";
+
+char mqtt_sub_topic_pattern[] = "+CMQTTRXTOPIC: %*d,%d\r\n"
+                                "%[^\r]\r\n"
+                                "+CMQTTRXPAYLOAD: %*d,%d\r\n"
+                                "%[^\r]\r\n"
+                                "+CMQTTRXEND: %d\r\n";
---
+++ main.cpp
@@ -0,0 +1,67 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "./TimerTPL5010/TimerTPL5010.h"
+#include "USBSerial.h"
+#include "devices_src.h"
+#include "mbed.h"
+#include <cstdint>
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE 500ms
+
+DigitalOut led(LED1);
+TimerTPL5010 tpl5010(WDT_WAKE_PIN, WDT_DONE_PIN);
+UnbufferedSerial ecard(ECARD_TX_PIN, ECARD_RX_PIN, 2400);
+USBSerial pc;
+
+Thread isr_thread(osPriorityAboveNormal, 0x400, nullptr, "isr_queue_thread");
+Thread usb_thread;
+
+EventQueue isr_queue;
+
+void on_rx_interrupt();
+
+void usb_task() {
+  printf("Start usb_task\r\n");
+
+  while (true) {
+    while (pc.connected()) {
+      if (pc.readable()) {
+        uint8_t byte = pc.getc();
+        // putc(byte, stdout);
+        ecard.write((void *)&byte, 1);
+      }
+    }
+  }
+}
+int main() {
+  // Initialise the digital pin LED1 as an output
+  printf("start Loopback_ecard\r\n");
+
+  isr_thread.start(callback(&isr_queue, &EventQueue::dispatch_forever));
+  tpl5010.init(&isr_queue);
+  ecard.attach(callback(on_rx_interrupt), SerialBase::RxIrq);
+  usb_thread.start(callback(usb_task));
+
+  while (true) {
+    if (tpl5010.get_wdt()) {
+      tpl5010.set_wdt(false);
+    }
+
+    led = !led;
+    ThisThread::sleep_for(BLINKING_RATE);
+  }
+}
+
+void on_rx_interrupt() {
+  char c;
+
+  // Read the data to clear the receive interrupt.
+  if (ecard.read(&c, 1)) {
+    // Echo the input back to the terminal.
+    pc.write((void *)&c, 1);
+  }
+}
